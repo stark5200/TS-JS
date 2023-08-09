d9ebd0c82a86d17bef6f7c369690e4fb24230d30
stark5200 <a.taha5200@gmail.com>
2023-08-09 20:38:10 +0200

    Numbers

---
+++ js1/JS/main.js
@@ -0,0 +1,16 @@
+console.log("Hiiiiiiiiiiiiiii")
+//strings
+const myVariable = "Mathematics";
+
+
+// string methods
+console.log(myVariable.length)
+console.log(myVariable.charAt(6))
+console.log(myVariable.indexOf('a'))
+console.log(myVariable.lastIndexOf('a'))
+console.log(myVariable.slice(2, 7))
+console.log(myVariable.toUpperCase())
+console.log(myVariable.includes("athe"))
+console.log(myVariable.split(""))
+
+//Numbers
