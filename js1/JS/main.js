6879ac176c31f68edb1d82f01c057a6b5c13bedd
stark5200 <a.taha5200@gmail.com>
2023-08-09 21:00:02 +0200

    Math

--- js1/JS/main.js
+++ js1/JS/main.js
@@ -14,3 +14,17 @@ console.log(myVariable.includes("athe"))
 console.log(myVariable.split(""))
 
 //Numbers
+const myNumber = 42;
+const myFloat = 42.514511;
+const myString = "42.151656bhhb";
+//.toFixed .parseFloat     Number.parseInt       NaN Not a Number
+
+const parsed = Number(parseFloat(myString).toFixed(2));  
+console.log(typeof(parsed))
+console.log(parsed + 15);
+console.log(myFloat.toFixed(2));
+
+// Math pow min max trunc round floor ceil random
+console.log(Math.PI)
+console.log(Math.trunc(Math.PI))
+console.log(Math.round(Math.PI, 3))
