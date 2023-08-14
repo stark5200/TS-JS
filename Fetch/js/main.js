be77b7c301bedfe39e7640428b60e52d65f9444b
stark5200 <a.taha5200@gmail.com>
2023-08-15 01:20:12 +0200

    cerial break

--- Fetch/js/main.js
+++ Fetch/js/main.js
@@ -24,20 +24,71 @@ const myPromise = new Promise((resolve, reject) => {
 const myNextPromise = new Promise((resolve, reject) => {
   setTimeout(function() {
     resolve("Mynext promise resolved");
-  }, 3000);
+  }, 300);
 }); 
 //console.log(myPromise);
 
 myNextPromise.then(value => {
-  console.log(value);
+  //console.log(value);
 });
 
 myPromise.then(value => {
   return value+1;
 }).then(newValue => {
-  console.log(newValue);
+  //console.log(newValue);
 }).catch(err => {
   console.error(err);
 })
 
+// pending
 
+const users = fetch("https://jsonplaceholder.typicode.com/users")
+.then(response => {
+  return response.json();
+})
+.then(data => {
+  data.forEach(user => {
+    //console.log(user);
+  })
+});
+
+//console.log(users);
+
+
+// Async / Await
+
+const myGuys = {
+  GuyList: []
+}
+
+const myCoolFunction = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const jsonGuyData = await response.json();
+  return jsonGuyData; 
+}
+
+myCoolFunction();
+
+const anotherFunc = async () => {
+  const data = await myCoolFunction();
+  myGuys.GuyList = data;
+  //console.log(data);
+} 
+
+anotherFunc();
+//console.log(myGuys.GuyList);
+
+//workflow function examples
+
+const getAllUserEmails = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const jsonUserData = await response.json();
+
+  const userEmailArray = jsonUserData.map(user => {
+    return user.email;
+  });
+
+  console.log(userEmailArray);
+}
+
+getAllUserEmails();
