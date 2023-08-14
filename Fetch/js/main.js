00c046f8b26fc435fc325eae15577cd41f46785c
stark5200 <a.taha5200@gmail.com>
2023-08-15 00:45:26 +0200

    smoke break

--- Fetch/js/main.js
+++ Fetch/js/main.js
@@ -1,2 +1,43 @@
 // Fetch Apis requires a discussion of ..
 // callbacks, Promises, Thenables and Async/Await
+
+// callbacks
+
+function firstFunction(paremeters, callback) {
+  // do stuff
+  callback()
+}
+
+// AKA callback hell
+
+// Promises
+// 3 states: Pending, fulfilled, Rejected
+const myPromise = new Promise((resolve, reject) => {
+  const error = true;
+  if (!error) {
+    resolve("Yes! Resolved the promise!");
+  } else {
+    resolve("No! Rejected the promise!");
+  }
+});
+
+const myNextPromise = new Promise((resolve, reject) => {
+  setTimeout(function() {
+    resolve("Mynext promise resolved");
+  }, 3000);
+}); 
+//console.log(myPromise);
+
+myNextPromise.then(value => {
+  console.log(value);
+});
+
+myPromise.then(value => {
+  return value+1;
+}).then(newValue => {
+  console.log(newValue);
+}).catch(err => {
+  console.error(err);
+})
+
+
