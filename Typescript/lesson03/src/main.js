0b8f96c5e0842f0e033b2ad5e2558b47fb46a3a5
stark5200 <a.taha5200@gmail.com>
2023-08-17 06:01:05 +0200

    ts arrays

---
+++ Typescript/lesson03/src/main.js
@@ -0,0 +1,3 @@
+var stringArr = ['one', 'hey', 'ammar'];
+var guitars = ['strat', 'les paul', 5150];
+var mixedData = ['EVH', 1984, true];
---
+++ Typescript/lesson03/src/main.ts
@@ -0,0 +1,5 @@
+let stringArr = ['one', 'hey', 'ammar']
+
+let guitars = ['strat', 'les paul', 5150]
+
+let mixedData = ['EVH', 1984, true]
