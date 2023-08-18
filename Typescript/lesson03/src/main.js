c50a4532f7f8ac4a2931c2b1fa7edd0653be5538
stark5200 <a.taha5200@gmail.com>
2023-08-18 04:52:56 +0200

    end of  objects

--- Typescript/lesson03/src/main.js
+++ Typescript/lesson03/src/main.js
@@ -1,3 +1,46 @@
 var stringArr = ['one', 'hey', 'ammar'];
 var guitars = ['strat', 'les paul', 5150];
 var mixedData = ['EVH', 1984, true];
+var test = [];
+var bands = [];
+// Tuple are sensitive to types and number of items
+var myTuple = ['Dave', 1950, false];
+var mixed = ['Dave', 1950, false];
+mixed.push('hey babe');
+//myTuple[3] = 'bloody'
+mixed = myTuple; // myTuple = mixed not possible 
+// objects
+var myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+var exampleObj = {
+    prop1: 'Dave',
+    prop2: true
+};
+exampleObj.prop2 = false;
+var evh = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812']
+};
+var jimmy = {
+    name: 'Jimmy',
+    albums: [194, 'OU82', 5]
+};
+var greetGuitarist = function (guitarist) {
+    return "Hello ".concat(guitarist.active ? guitarist.active : "");
+};
+console.log(greetGuitarist(jimmy));
+// Enums 
+// added to the languange and runtime
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 2] = "U";
+    Grade[Grade["D"] = 3] = "D";
+    Grade[Grade["C"] = 4] = "C";
+    Grade[Grade["B"] = 5] = "B";
+    Grade[Grade["A"] = 6] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.C);
--- Typescript/lesson03/src/main.ts
+++ Typescript/lesson03/src/main.ts
@@ -3,3 +3,71 @@ let stringArr = ['one', 'hey', 'ammar']
 let guitars = ['strat', 'les paul', 5150]
 
 let mixedData = ['EVH', 1984, true]
+
+let test = []
+let bands: string[] = []
+
+// Tuple are sensitive to types and number of items
+let myTuple: [string, number, boolean] = ['Dave', 1950, false]
+let mixed = ['Dave', 1950, false]
+mixed.push('hey babe')
+//myTuple[3] = 'bloody'
+mixed = myTuple // myTuple = mixed not possible 
+
+
+// objects
+let myObj: object
+myObj = []
+console.log(typeof myObj)
+myObj = bands
+myObj = {}
+
+const exampleObj = {
+  prop1: 'Dave',
+  prop2: true
+
+}
+ 
+exampleObj.prop2 = false
+
+type Guitarist = {
+  name: string,
+  active?: boolean,
+  albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+  name: 'Eddie',
+  active: false, 
+  albums: [1984, 5150, 'OU812']
+}
+
+let jimmy: Guitarist = {
+  name: 'Jimmy',
+  albums: [194, 'OU82', 5]
+}
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  return `Hello ${guitarist.active? guitarist.active : ""}`
+}
+
+console.log(greetGuitarist(jimmy))
+
+interface Pianist  {
+  name: string,
+  active?: boolean,
+  albums: (string | number)[]
+}
+
+// Enums 
+// added to the languange and runtime
+
+enum Grade {
+  U = 2, 
+  D,
+  C,
+  B, 
+  A, 
+}
+
+console.log(Grade.C)
