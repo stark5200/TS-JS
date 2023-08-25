f35ec530b216620338756eac730d3a9b1dd093c4
stark5200 <a.taha5200@gmail.com>
2023-08-26 00:21:30 +0200

    end of 12

--- Typescript/React1/src/App.tsx
+++ Typescript/React1/src/App.tsx
@@ -1,9 +1,19 @@
 import Heading from "./components/Heading"
+import { Section } from "./components/Section"
+import  Counter  from "./components/Counter"
+
+import { useState } from "react"
 
 function App() {
 
+  const [count, setCount] = useState<number>(1)
+
   return (
-    <Heading title = {"Hello"}/>
+    <>
+      <Heading title = {"Hello"}/>
+      <Section title="New Title">This is my Section.</Section>
+      <Counter setCount={setCount}>Count is {count}</Counter>
+    </>
   )
 }
 
