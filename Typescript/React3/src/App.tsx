f90d77d27f2049c82d0bbcbee3502eb025c646cb
stark5200 <a.taha5200@gmail.com>
2023-08-26 07:03:09 +0200

    end of react 4

--- Typescript/React3/src/App.tsx
+++ Typescript/React3/src/App.tsx
@@ -1,12 +1,18 @@
 import Counter from "./Counter"
+import { CounterProvider } from "./context/CounterContext"
+import { initState } from "./context/CounterContext"
 
 function App() {
 
   return (
     <>
-      <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+      <CounterProvider count={initState.count} text={initState.text}>
+        <Counter>{(num: number) => <>Current Count: {num}</>}</Counter>
+      </CounterProvider>
     </>
   )
 }
+
+
 
 export default App
