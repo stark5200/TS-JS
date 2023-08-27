870f162ca2218378da1866296f018f01db92fd69
stark5200 <a.taha5200@gmail.com>
2023-08-27 05:26:45 +0200

    Shopping Cart

--- Typescript/ReactFinal/src/App.tsx
+++ Typescript/ReactFinal/src/App.tsx
@@ -1,14 +1,24 @@
+import Header from "./components/Header" 
+import Footer from "./components/Footer" 
+import Cart from "./components/Cart" 
+import ProductList from "./components/ProductList" 
 
+import { useState } from "react"
 
 function App() {
+  const [viewCart, setViewCart] = useState(false)
 
-  return (
+  const pageContent = viewCart ? <Cart /> : <ProductList />
+
+  const content = (
     <>
-      <div className="App">
-        
-      </div>
+      <Header viewCart={viewCart} setViewCart={setViewCart} />
+      {pageContent}
+      <Footer viewCart={viewCart} />
     </>
   )
+
+  return content
 }
 
 export default App
